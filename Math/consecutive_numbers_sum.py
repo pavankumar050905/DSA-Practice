89112bd6e84117a3f6c511f0aaac6999ab5bf16a
pavankumar050905 <pbandarubtech24@ced.alliance.edu.in>
2026-03-18 22:57:04 +0530

    Solved Consecutive Numbers Sum problem (LeetCode)
    Implement a solution to count consecutive numbers that sum to a given integer n.

---
+++ Math/consecutive_numbers_sum.py
@@ -0,0 +1,14 @@
+# Problem: Consecutive Numbers Sum
+# Platform: LeetCode
+
+class Solution:
+    def consecutiveNumbersSum(self, n: int) -> int:
+        count = 0
+        k = 1
+        
+        while k * (k - 1) // 2 < n:
+            if (n - k * (k - 1) // 2) % k == 0:
+                count += 1
+            k += 1
+        
+        return count
